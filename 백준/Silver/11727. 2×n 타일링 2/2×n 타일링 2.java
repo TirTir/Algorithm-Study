1ea2bc178de1db7daed813b14d04d520b25b71ad
TirTir <73577283+TirTir@users.noreply.github.com>
2024-10-17 14:24:40 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public int solution(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 3;
+
+        int[] dp = new int[n + 1];
+
+        // dp 초기화
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String [] args) throws IOException{
+        Main T = new Main();
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = T.solution(n);
+        System.out.println(answer);
+    }
+}
