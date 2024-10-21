8c4db2e9d1c5c20b396352796280b19dee3a7663
TirTir <73577283+TirTir@users.noreply.github.com>
2024-10-21 17:23:17 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 14716 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] dp, price;
+
+    public void solution(int n){
+        for (int i = 1; i <= n; i++) {
+            dp[i] = price[i - 1];
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + price[j-1]);
+            }
+        }
+    }
+
+    public static void main(String [] args) throws IOException{
+        Main T = new Main();
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+        price = Arrays.stream(br.readLine().split(" "))
+                      .mapToInt(Integer::parseInt)
+                      .toArray();
+
+        T.solution(n);
+        System.out.println(dp[n]);
+    }
+}
