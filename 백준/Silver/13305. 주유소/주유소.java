6c8950ade4f8f126170b73311537e8486cf29b4f
TirTir <73577283+TirTir@users.noreply.github.com>
2024-11-17 02:54:39 +0900

    [Silver III] Title: 주유소, Time: 412 ms, Memory: 39456 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -6,23 +6,19 @@ public class Main {
 
     public static void main(String [] args) throws IOException{
 			int n = Integer.parseInt(br.readLine());
-			long[] price = new long[n];
-			int answer = 0;
-			
 			long[] len = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] price = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++){
-				price[i] = Integer.parseInt(st.nextToken());
-			}
+        long totalCost = 0;
+        long minPrice = price[0];
 
-			long minPrice = price[0];
-			for(int i = 0; i < len.length; i++){
-				if (price[i] < minPrice) {
-					minPrice = price[i];
-				}
-				answer += minPrice * len[i];
-			}
-			System.out.println(answer);
+        for (int i = 0; i < len.length; i++) {
+            totalCost += minPrice * len[i];
+            if (price[i + 1] < minPrice) {
+                minPrice = price[i + 1];
+            }
+        }
+
+        System.out.println(totalCost);
 		}
 }
