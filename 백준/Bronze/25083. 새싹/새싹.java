9f38b95e8f97854900a8a18a5d5628cb68809e48
TirTir <73577283+TirTir@users.noreply.github.com>
2024-11-21 02:19:48 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+  static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String [] args) throws IOException{
+		StringBuffer sb = new StringBuffer();
+		sb.append("         ").append(",r'\"7").append("\n");
+		sb.append("r`-_   ,'  ,/").append("\n");
+		sb.append(" \\. \". L_r'").append("\n");
+    sb.append("   `~\\/").append("\n");
+		sb.append("      |").append("\n");
+    sb.append("      |");
+		
+		System.out.println(sb.toString());
+	}
+}
