6911c438d3f799d7c321491cca729a44b84bf54f
TirTir <73577283+TirTir@users.noreply.github.com>
2024-12-02 00:01:49 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 20156 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,21 +5,24 @@ public class Main {
   static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
   public static void main(String [] args) throws IOException{
-    int n = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine()); // N개
     int m = Integer.parseInt(br.readLine()); // 길이
     String s = br.readLine();
 
-    StringBuilder sb = new StringBuilder("I");
-    for (int i = 0; i < n; i++) {
-      sb.append("OI");
-    }
-
-    String str = sb.toString();
-    int answer = 0;
-
-    for(int i = 0; i <= m - str.length(); i++){     
-      if(s.substring(i, i + str.length()).equals(str)){
-        answer++;
+    int cnt = 0, answer = 0;
+    for(int i = 1; i < m-1;){
+      if(s.charAt(i) == 'O' && s.charAt(i+1) == 'I'){
+        cnt++;
+        if(cnt == n){
+          if(s.charAt(i - (n*2 - 1)) == 'I'){
+            answer++;
+          }
+          cnt--;
+        }
+        i += 2; // O 부터 비교
+      } else {
+        cnt = 0;
+        i++;
       }
     }
 
