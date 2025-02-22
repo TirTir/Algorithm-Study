dc5d7387c12635ea442f470575eb14914a023ea8
TirTir <73577283+TirTir@users.noreply.github.com>
2025-02-22 15:23:55 +0900

    [level 2] Title: 점 찍기, Time: 16.04 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for (int x = 0; x <= d; x += k) {
+            long max = (long) Math.sqrt((long) d * d - (long) x * x);
+            answer += (max / k) + 1;
+        }
+        
+        return answer;
+    }
+}
